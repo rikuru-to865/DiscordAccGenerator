f541ca5dc1d587c85eaef9151bbf520b7cd2d325
rikuru_to <usaginosipppo865@gmail.com>
2021-02-22 13:50:21 +0900

    kinisuruna

--- DAC.py
+++ DAC.py
@@ -7,7 +7,7 @@ dicordPasswordBoxName = "password"
 BornYear = "react-select-2-input"
 BornMonth = "react-select-3-input"
 BornDay = "react-select-4-input"
-driver = webdriver.Chrome(executable_path='./chromedriver')
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 def WriteForm(Email,Username,Password,Bornyear,Bornmonth,Bornday):
     driver.get('https://discord.com/register')
     Mailbox = driver.find_element_by_name("email")
@@ -31,6 +31,6 @@ def WriteForm(Email,Username,Password,Bornyear,Bornmonth,Bornday):
 
     BornDayC.submit()
 
-    driver.delete_all_cookies()
+
 if __name__ == '__main__':
     sys.exit()
